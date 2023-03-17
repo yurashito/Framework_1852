69aa6aeff122e8bc4ea2c74b6db0c60c803fccaa
yurashito <118957930+yurashito@users.noreply.github.com>
2023-03-17 17:47:35 +0100

    sprint2

---
+++ WEB-INF/classes/Mapping.java
@@ -0,0 +1,20 @@
+package etu1852.framework;
+
+public class Mapping{
+    String ClassName ;
+    String Method;
+
+    public String getClassName(){
+        return this.ClassName;
+    }
+    public void setClassName( String classeName){
+        this.ClassName=classeName;
+    }
+    public String getMethod(){
+        return this.Method;
+    }
+    public void setMethod( String Method){
+        this.ClassName=Method;
+    }
+
+}
