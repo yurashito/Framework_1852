0e7d55a9c919d8ad5ac84aa5de898aa73b0606ac
yurashito <118957930+yurashito@users.noreply.github.com>
2023-07-08 00:20:03 +0200

    Sprint11

--- framework/WEB-INF/classes/Utilitaire.java
+++ framework/WEB-INF/classes/Utilitaire.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.FileOutputStream;
+import java.lang.reflect.*;
 public class Utilitaire{
     public String url(String url) throws Exception{    
         String supprime_le_baseurl=url.split("\\?")[0];
@@ -76,6 +77,47 @@ public class Utilitaire{
         }
         return false;
         
+    }
+
+    public void mettre_par_defaut_les_attributs(Field[] attributs , Object objet) throws Exception{
+        for(int i=0 ; i< attributs.length ; i++){
+            attributs[i].setAccessible(true);
+            if(attributs[i].getType().getSimpleName().equals("int")){
+                attributs[i].set(objet , 0);
+            }else if( attributs[i].getType().getSimpleName().equals("String")){
+                attributs[i].set(objet , new String());
+            }else if( attributs[i].getType().getSimpleName().equals("double")){
+                attributs[i].set(objet , 0);
+            }
+        }
+    }
+    public void donne_un_valeur_a_un_attribut( String nom_attribut , Class type_attribut , Object objet , String valeur , Object connecte , Object profil ) throws Exception{
+            Method meth;
+            Class classAuth= Class.forName("etu1852.annotation.Auth");
+            if(type_attribut.getSimpleName().equals("int")){ 
+                    meth= objet.getClass().getMethod("set"+nom_attribut ,type_attribut); 
+                        
+                        meth.invoke(objet , Integer.parseInt(valeur));
+    
+                    
+                } else if(type_attribut.getSimpleName().equals("String")){ 
+                    meth= objet.getClass().getMethod("set"+nom_attribut ,type_attribut);
+                    meth.invoke(objet , valeur);
+                } else if(type_attribut.getSimpleName().equals("double")){  
+                    meth= objet.getClass().getMethod("set"+nom_attribut ,type_attribut);
+                    meth.invoke(objet , Double.parseDouble(valeur));
+                } else { 
+                    meth= objet.getClass().getMethod("set"+nom_attribut ,type_attribut);
+                    meth.invoke(objet , string_en_date(valeur));
+                }
+    }
+
+    public boolean verificationProfil(Method meth )throws Exception{
+        Class classAuth= Class.forName("etu1852.annotation.Auth");
+        if(meth.isAnnotationPresent(classAuth)){
+            return true ;
+        }
+        return false;
     }
 
     
--- testFramework/index.jsp
+++ testFramework/index.jsp
@@ -1,3 +1,13 @@
+<% if( request.getAttribute("liste") !=null){
+        out.println(request.getAttribute("liste")); 
+          
+    }
+    String a = getServletConfig().getInitParameter("isConnected");
+%>
+<%= a  %>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,15 +17,13 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Accueil</h1>
     <a href="TestFramework/emp-all"> lien11</a>
-    <form action="http://localhost:8082/testFramework6/select.do">
-        <p>Id: <input type="text" name="Id" ></p>
-        <p>Nom: <input type="text" name="Nom" ></p>
+    <form action="http://localhost:8082/test_framework7/up.do">
+        <p>Id: <input type="number" name="A" ></p>
+        <p>Nom: <input type="number" name="B" ></p>
         <p> <input type="submit" value="ok" ></p>
     </form>
-
-
-
     <a href="emp-all.do?hihi=1"> lien11</a>
 </body>
 </html>
